eb7a02abfccdf49b30ff12cf0a4b3a0f6cdc76e8
Hunter Tunnicliff <hunter@tunnicliff.co>
2022-05-11 16:07:39 +0000

    Update Jest

--- jest.config.ts
+++ jest.config.ts
@@ -5,6 +5,9 @@ const config: Config.InitialOptions = {
   setupFiles: ["./jest.pre-setup.ts"],
   setupFilesAfterEnv: ["./jest.setup.ts"],
   verbose: true,
+  fakeTimers: {
+    enableGlobally: true,
+  },
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1",
   },
